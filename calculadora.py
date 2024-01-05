1b6fdb5b6e50ad18009308b4441134b94209107b
Estudiante Ltic <estudiante@sistemas.local>
2024-01-05 00:54:22 -0500

    Initial commit

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+
+def suma (a, b, c):
+    return a + b + c 
+
+def resta (a, b):
+
+    return a - b
+
+def multiplicacion (a, b):
+
+    return a * b 
+
+def division (a, b):
+    if b != 0:
+        return a / b
+    else:
+        raise ValueError("No se puede dividir por cero")
